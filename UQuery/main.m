66f4ff8730befd64fb2a5927f36cee9b639eef0b
Frank <frank@iMac-bigdata.local>
2014-07-17 11:04:26 +0800

    Initial Commit

---
+++ UQuery/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UQuery
+//
+//  Created by Frank on 14-7-17.
+//  Copyright (c) 2014年 bigdata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
